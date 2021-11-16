03810b15287e8e70079de3c2123ec47694ea361a
Nisarg1999 <42809495+Nisarg1999@users.noreply.github.com>
2021-11-16 15:54:22 +0530

    Add files via upload

---
+++ data_manager.py
@@ -0,0 +1,27 @@
+import requests
+from pprint import pprint
+SHEETY_ENDPOINT = "https://api.sheety.co/c0b75736636231548c8b6f5e7205f9c6/flightDeals/prices"
+
+class DataManager:
+    #This class is responsible for talking to the Google Sheet.
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
